103327ede852813b917e2698222dcb2d789a98b1
skruban <uisudhan@gmail.com>
2020-03-20 23:49:11 +0530

    updated initial state

--- src/reducer.js
+++ src/reducer.js
@@ -1,4 +1,20 @@
-export default (state = {}, action) => {
+const initialState = {
+  title: 'React Redux Todo App',
+  tasks: [
+    {
+      isCompleted: false,
+      name: 'read c program',
+      id: 'task-1'
+    },
+    {
+      isCompleted: true,
+      name: 'read bfeg-211',
+      id: 'task-2'
+    }
+  ]
+};
+
+export default (state = initialState, action) => {
   switch (action.type) {
     default:
       return state;
